873920422d61d747e6374d4ff3f753839a9da420
Eric Bohlman <ebohlman@omsdev.com>
2001-11-20 03:53:37 +0000

    Import of EBOHLMAN/XML-Handler-Trees-0.02 from CPAN.
    gitpan-cpan-distribution: XML-Handler-Trees
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         EBOHLMAN/XML-Handler-Trees-0.02.tar.gz
    gitpan-cpan-author:       EBOHLMAN
    gitpan-cpan-maturity:     released

--- Trees.pm
+++ Trees.pm
@@ -2,7 +2,7 @@ use strict;
 
 package XML::Handler::Trees;
 use vars qw/$VERSION/;
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 package XML::Handler::Tree;
 
@@ -20,14 +20,31 @@ sub start_document {
 sub start_element {
   my ($self,$element)=@_;
   my $newlist;
-  if (ref $element->{Attributes} eq 'HASH') {
+  if (exists $element->{LocalName}) {
+    # namespaces are available!
+    $newlist = [{}];
+    foreach my $attr (values %{$element->{Attributes}}) {
+      if ($attr->{NamespaceURI}) {
+        $newlist->[0]{"{$attr->{NamespaceURI}}$attr->{LocalName}"} = $attr->{Value};
+      }
+      else {
+        $newlist->[0]{$attr->{Name}} = $attr->{Value};
+      }
+    }
+  }
+  elsif (ref $element->{Attributes} eq 'HASH') {
     $newlist=[{map {$_=>$element->{Attributes}{$_}} keys %{$element->{Attributes}}}];
   }
   else {
     $newlist=[{map {$_=>$element->{Attributes}{$_}{Value}} keys %{$element->{Attributes}}}];
   }
   push @{ $self->{Lists} }, $self->{Curlist};
-  push @{ $self->{Curlist} }, $element->{Name} => $newlist;
+  if (exists($element->{LocalName}) && $element->{NamespaceURI}) {
+    push @{ $self->{Curlist} }, "{$element->{NamespaceURI}}$element->{LocalName}" => $newlist;
+  }
+  else {
+    push @{ $self->{Curlist} }, $element->{Name} => $newlist;
+  }
   $self->{Curlist} = $newlist;
 }
 
@@ -61,12 +78,11 @@ sub end_document {
 
 package XML::Handler::EasyTree;
 
-use vars qw($Noempty $Latin);
-
 sub new {
   my $class=shift;
   $class=ref($class) || $class;
-  my $self={Noempty=>0,Latin=>0,@_};
+  my $self={Noempty=>0,Latin=>0,Searchable=>0,@_};
+  $self->{Noempty}||=$self->{Searchable};
   bless $self,$class;
 }
 
@@ -80,8 +96,19 @@ sub start_element {
   my ($self,$element)=@_;
   $self->checkempty();
   my $newlist=[];
-  my $newnode={type=>'e',attrib=>{},name=>$self->nsname($element),content=>$newlist};
-  if (ref $element->{Attributes} eq 'HASH') {
+  my $newnode;
+  if ($self->{Searchable}) {
+    $newnode= XML::Handler::EasyTree::Searchable->new( Name => $self->nsname($element), Content => $newlist );
+  }
+  else {
+    $newnode={type=>'e',attrib=>{},name=>$self->nsname($element),content=>$newlist};
+  }
+  if (exists $element->{LocalName}) {
+    while (my ($name,$obj) = each %{$element->{Attributes}}) {
+      $newnode->{attrib}{$name} = $self->encode($obj->{Value});
+    }
+  }
+  elsif (ref $element->{Attributes} eq 'HASH') {
     while (my ($name,$val)=each %{$element->{Attributes}}) {
       $newnode->{attrib}{$self->nsname($name)}=$self->encode($val);
     }
@@ -125,6 +152,9 @@ sub end_document {
   $self->checkempty();
   delete $self->{Curlist};
   delete $self->{Lists};
+  if ($self->{Searchable}) {
+    return XML::Handler::EasyTree::Searchable->new( Name => '__TOPLEVEL__', Content => $self->{Tree} );
+  } 
   $self->{Tree};
 }
 
@@ -163,6 +193,269 @@ sub checkempty() {
   }
 }
 
+package XML::Handler::EasyTree::Searchable;
+
+#
+# new() returns a new node with the same structure at the `newnode'
+# hashref
+#
+# Usage: XML::Handler::EasyTree::Searchable->new( Name => $name, Content => $content );
+#
+sub new {
+  my $type = shift;
+  my $class = ref($type) || $type || die "must supply a object type" ;
+
+  my %opts = @_;
+
+  my $name = $opts{Name} || '';
+  my $content = $opts{Content} || undef;
+
+  return bless ( {
+		  type    => 'e',
+		  attrib  => {},
+		  name    => $name, 
+		  content => $content,
+		 }, $class);
+}
+
+#
+# name() returns the name of the node. Ideally, it should return a
+# "fully qualified" name, but it doesn't
+#
+sub name {
+  my $self = shift;
+  return $self->{name};
+}
+
+#
+# value() returns the value associated with an object
+#
+sub value {
+  my $self = shift;
+
+  return( undef )
+    unless( ( exists $self->{content} ) && ( defined $self->{content} ) );
+
+  my $possible = $self->{content};
+
+  die "not an array" unless( "$possible" =~ /ARRAY/ );
+
+  $possible = $possible->[0];
+
+  return( undef )
+    unless( ( exists $possible->{type} ) && ( $possible->{type} eq 't' ) );
+
+  return( undef )
+    unless( ( exists $possible->{content} ) && ( defined $possible->{content} ) );
+
+  return $possible->{content};
+}
+
+#
+# usage: $newobj = $obj->child( $name );
+#
+# child() returns a child (elements only) of the object with the $name
+#
+# for the case where there is more than one child that match $name,
+# the array context semantics haven't been completely worked out:
+# - in an array context, all children are returned.
+# - in scalar context, the first child matching $name is returned.
+#
+# In a scalar context, The XML::Parser::SimpleObj class returns an
+# object containing all the children matching $name, unless there is
+# only one child in which case it returns that child (see commented
+# code). I find that behavior confusing.
+# 
+sub child {
+  my $self = shift;
+  my $spec = shift || '';
+
+  my $array = $self->{content};
+
+  my @rv;
+  if( $spec ) {
+    @rv = grep { $_->{name} eq $spec } grep { $_->{type} eq 'e' } @$array;
+  } else {
+    @rv = grep { $_->{type} eq 'e' } @$array;
+  }
+
+  my $num = scalar( @rv );
+  
+  if( wantarray() ) {
+    return @rv; 
+  } else {
+    return '' unless( $num );
+    return $rv[0] if( $num == 1 );
+    # my $class = ref( $self );
+    # return $class->new( Name => "__magic_child_list_object__", Content => [ @rv ] );
+  }
+}
+
+#
+# usage: @children = $obj->children( $name );
+#
+# children() returns a list of all children (elements only) of the
+# $obj that match $name -- in the order in which they appeared in the
+# original xml text.
+#
+sub children {
+  my $self = shift;
+  my $array = $self->{content};
+  my $spec = shift || '';
+
+
+  my @rv;
+  if( $spec ) {
+    @rv = grep { $_->{name} eq $spec } grep { $_->{type} eq 'e' } @$array;
+  } else {
+    @rv = grep { $_->{type} eq 'e' } @$array;
+  }
+
+  return @rv;
+}
+
+#
+# usage: @children_names = $obj->children_names();
+#
+# children_names() returns a list of all the names of the objects
+# children (elements only) in the order in which they appeared in the
+# original text
+#
+sub children_names {
+  my $self = shift;
+  my $array = $self->{content};
+
+  return map { $_->{name} } grep { $_->{type} eq 'e' } @$array;
+}
+
+#
+# usage: $attrib = $obj->attribute( $att_name );
+#
+# attribute() returns the string associated with the attribute of the
+# object. If not found returns a null string.
+#
+sub attribute {
+  my $self = shift;
+  my $spec = shift || return '';
+
+  return '' unless( ( exists $self->{attrib} ) && ( defined $self->{attrib} ) );
+
+  my $attrib = $self->{attrib};
+  return '' unless( ( exists $attrib->{$spec} ) && ( defined $attrib->{$spec} ) );
+
+  return $attrib->{$spec};
+}
+  
+#
+# usage: @attribute_list = $obj->attribute_list();
+#
+# attribute_list() returns a list (in no particular order) of the
+# attribute names associated with the object
+#
+sub attribute_list {
+  my $self = shift;
+
+  return '' unless( ( exists $self->{attrib} ) && ( defined $self->{attrib} ) );
+
+  my $attrib = $self->{attrib};
+  return '' unless( "$attrib" =~ /HASH/ );
+
+  return keys %$attrib;
+}
+
+#
+# usage: $text = $obj->dump_tree();
+#
+# dump_tree() returns a textual representation (in xml form) of the
+# object's heirarchy. Only elements are processed.
+#
+#
+sub dump_tree {
+  my $self = shift;
+  my %opts = @_;
+
+  my $pretty = delete $opts{-pretty};
+
+  my $name	= $self->name();
+  my $value	= $self->value();
+  my @children	= $self->children();
+
+  my $text = '';
+  unless( $name eq '__TOPLEVEL__' ) {
+      $text .= "<$name";
+      for my $att ( $self->attribute_list() ) {
+	  $text .= sprintf( " %s=\"%s\"", $att, encode($self->attribute( $att )) );
+      }
+      $text .= ">";
+
+      if( $value ) {
+	  $text .= encode($value);
+      }
+  }
+
+  
+  for my $child ( @children ) {
+    $text .= $child->dump_tree();
+  }
+
+  unless( $name eq '__TOPLEVEL__' ) {
+      $text .= "</$name>";
+  }
+
+  return $text;
+}
+
+#
+# usage: $text = $obj->pretty_dump_tree();
+#
+# pretty_dump_tree() is identical to dump_tree(), except that newline
+# and indentation embellishments are added
+#
+sub pretty_dump_tree {
+  my $self = shift;
+  my $tab = shift || 0;
+
+  my $indent = " " x ( 2 * $tab );
+
+  my $name	= $self->name();
+  my $value	= $self->value();
+  my @children	= $self->children();
+
+  my $text = '';
+  unless( $name eq '__TOPLEVEL__' ) {
+      $text .= "$indent<$name";
+      for my $att ( $self->attribute_list() ) {
+	  $text .= sprintf( " %s=\"%s\"", $att, encode($self->attribute( $att )) );
+      }
+      $text .= ">";
+      
+      if( defined $value ) {
+	  $text .= encode($value);
+	  $text .= "</$name>\n";
+	  return $text;
+      } else {
+	  $text .= "\n";
+      }
+  }
+
+  for my $child ( @children ) {
+    $text .= $child->pretty_dump_tree( $tab + 1 );
+  }
+
+  unless( $name eq '__TOPLEVEL__' ) {
+      $text .= "$indent</$name>\n";
+  }
+
+  return $text;
+}
+
+sub encode {
+  my $encstr=shift;
+  my %encodings=('&'=>'amp','<'=>'lt','>'=>'gt','"'=>'quot',"'"=>'apos');
+  $encstr=~s/([&<>"'])/&$encodings{$1};/g;
+  $encstr;
+}
+
 package XML::Handler::TreeBuilder;
 
 use vars qw(@ISA);
@@ -185,7 +478,10 @@ sub start_document {}
 sub start_element {
   my ($self,$element)=@_;
   my @attlist;
-  if (ref $element->{Attributes} eq 'HASH') {
+  if (exists $element->{LocalName}) {
+    @attlist=map {$_=>$element->{Attributes}{$_}{Value}} keys %{$element->{Attributes}};
+  }
+  elsif (ref $element->{Attributes} eq 'HASH') {
     @attlist=map {$_=>$element->{Attributes}{$_}} keys %{$element->{Attributes}};
   } 
   else {
@@ -322,9 +618,9 @@ for processing instruction nodes. All nodes also have a 'content' key
 whose value is a reference to an array holding the element's child nodes
 for element nodes, the string value for text nodes, and the data value
 for processing instruction nodes. Element nodes also have an 'attrib'
-key whose value is a reference to a hash of attribute names and values.
-Processing instructions also have a 'target' key whose value is the PI's
-target. 
+key whose value is a reference to a hash of attribute names and values and a 'name'
+key whose value is the element's name.  Processing instructions also have
+a 'target' key whose value is the PI's target. 
 
 EasyTree nodes are ordinary Perl hashes and are not objects.  Contiguous 
 runs of text are always returned in a single node.
@@ -360,9 +656,120 @@ If this is set to a true value, characters with Unicode values in the
 Latin-1 range (160-255) will be stored in the tree as Latin-1 rather
 than UTF-8.  The default is false.
 
+=item Searchable
+
+If this is set to a true value, the parser will return a tree of XML::Handler::EasyTree::Searchable
+objects rather than bare array references, providing access to the navigation methods
+listed below.  The top-level node returned will be a dummy element node with a name of "__TOPLEVEL__".
+It is false by default.  Setting this option automatically enables the Noempty option.
+
 =back
 
 =back
+
+=head2 XML::Handler::EasyTree::Searchable METHODS
+
+If the Searchable option is set, all nodes in the tree will be XML::Handler::EasyTree::Searchable objects,
+which have the same structure as EasyTree nodes but also implement the following methods similar to
+those in XML::SimpleObject.
+
+=over 4
+
+=item $name = $node->name()
+
+Returns the name of the node. Ideally, it should return a
+"fully qualified" name, but it doesn't.
+
+=item $val = $node->value()
+
+Returns the text value associated with a node object.  Returns undef if the node has
+no text children or its first child is not a text node.
+
+=item $newobj = $obj->child( $name );
+
+Returns a child (elements only) of the object with the $name.
+
+For the case where there is more than one child that match $name,
+the array context semantics haven't been completely worked out:
+- in an array context, all children are returned.
+- in scalar context, the first child matching $name is returned.
+
+In a scalar context, The XML::Parser::SimpleObj class returns an
+object containing all the children matching $name, unless there is
+only one child in which case it returns that child (see commented
+code). I find that behavior confusing.
+
+=item @children = $obj->children( $name );
+
+Returns a list of all children (elements only) of the
+$obj that match $name -- in the order in which they appeared in the
+original xml text.
+
+=item @children_names = $obj->children_names();
+
+Returns a list of all the names of the objects
+children (elements only) in the order in which they appeared in the
+original text.
+
+=item $attrib = $obj->attribute( $att_name );
+
+Returns the string associated with the attribute of the
+object. If not found returns a null string.
+
+=item @attribute_list = $obj->attribute_list();
+
+Returns a list (in no particular order) of the
+attribute names associated with the object
+
+=item $text = $obj->dump_tree();
+
+Returns a textual representation (in xml form) of the
+object's hierarchy. Only elements are processed. The result will
+be in whatever character encoding the SAX driver delivered (which may
+not be the same encoding as the original source).
+
+=item $text = $obj->pretty_dump_tree();
+
+Identical to dump_tree(), except that newline
+and indentation embellishments are added
+
+=back
+
+=head2 EXAMPLE
+
+ #! /usr/bin/perl -w
+ 
+ use XML::Handler::Trees;
+ use XML::Parser::PerlSAX;
+ use strict;
+ 
+ my $p=XML::Parser::PerlSAX->new();
+ my $h=XML::Handler::EasyTree->new( Searchable=>1 );
+ my $easytree=$p->parse( Handler => $h, Source => { SystemId => 'systemB.xml' } );
+ 
+ my $vme = $easytree->child( "vmesystem" );
+ 
+ print "\n";
+ print "vmesystem config: ", $vme->attribute( "configuration_name" ), "\n";
+ 
+ print "\n";
+ print "vmesystem children: ", join( ', ', $vme->children_names() ), "\n";
+ 
+ print "\n";
+ print "gps model is ", $vme->child( "gps" )->child( "model" )->value(), "\n";
+ my $gps = $vme->child( "gps" );
+ print "gps slot is ", $gps->child( "slot" )->value(), "\n";
+ 
+ print "\n";
+ print "reconstructed XML: \n";
+ print $easytree->dump_tree(), "\n";
+ 
+ # print "\n";
+ # print "recontructed XML (pretty): \n";
+ # print $easytree->pretty_dump_tree(), "\n";
+ 
+ print "\n";
+ exit;
 
 =head1 CLASS XML::Handler::TreeBuilder
 
@@ -402,9 +809,18 @@ Currently, this is off (false) by default.
 
 Eric Bohlman (ebohlman@omsdev.com)
 
-Copyright (c) 2001 Eric Bohlman. All rights reserved. This program
-is free software; you can redistribute it and/or modify it under the same
-terms as Perl itself.
+PerlSAX 2 compatibility added by Matt Sergeant (matt@sergeant.org)
+
+XML::EasyTree::Searchable written by Stuart McDow (smcdow@moontower.org)
+
+Copyright (c) 2001 Eric Bohlman.
+
+Portions of this code Copyright (c) 2001 Matt Sergeant.
+
+Portions of this code Copyright (c) 2001 Stuart McDow.
+
+All rights reserved. This program is free software; you can redistribute it
+and/or modify it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
